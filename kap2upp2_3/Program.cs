4d030b7aa89d748957e8a4ff0c12d18579bba845
hulj07001 <hulj07001@E-PW05CGR7>
2024-10-10 12:14:46 +0200

    Add project files.

---
+++ kap2upp2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// int sum = 300 + (1 * km) + ((antal dagar - 1) * 500)
+//          start   1kr/km       minus första dagen
+using System;
+namespace kap2upp2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Välkommen till vår biluthyrningsfirma");
+            Console.WriteLine("Hur många dagar vill du hyra en bil? (Startavgift 300kr):");
+            string antaldagar = Console.ReadLine();
+            int intdagar = int.Parse(antaldagar);
+            Console.WriteLine("Hur många kilometer kommer du att köra?:");
+            string antalkm = Console.ReadLine();
+            double doublekm = double.Parse(antalkm);
+            double sum = 300 + (1 * doublekm) + ((intdagar - 1) * 500);
+            Console.WriteLine("Det kommer att kosta " + sum + "kr " + "att hyra bilen");
+        }
+    }
+}
